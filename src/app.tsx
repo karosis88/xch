6d6d89c7c0fcbd08ef5d9b2c310cb773e63a0fbd
karosis88 <kar.petrosyanpy@gmail.com>
2023-09-15 18:14:07 +0300

    Add special-for-khacik

--- src/app.tsx
+++ src/app.tsx
@@ -4,5 +4,8 @@ import Encoder from "./components/encoder"
 export default function App() {
     return <div className="app">
         <Encoder/>
+        <h3 className="special-for-khachik">
+            Հատուկ Խաչիկ Սարգսյանի համար
+        </h3>
     </div>
 }
