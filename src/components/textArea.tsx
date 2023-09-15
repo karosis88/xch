687d16c3931deebe7d79e9bbbf04582cc9ad2657
karosis88 <kar.petrosyanpy@gmail.com>
2023-09-15 18:38:09 +0300

    Add copy button

--- src/components/textArea.tsx
+++ src/components/textArea.tsx
@@ -6,10 +6,13 @@ type textAreaProps = {
 
 export default function TextArea({title, value, onChange}: textAreaProps) {
 
+
     return (<div>
             <div className="title-container">
                 <h3 className="title">{title}</h3>
-                {/* <img src={"../static/copy.png"}></img> */}
+                <a onClick={() => navigator.clipboard.writeText(value)}>
+                    <img className="copy" src="/xch/copy.png" alt="copy"></img>
+                </a>
             </div>
             <textarea placeholder="Enter text" value={value} onChange={onChange}></textarea>
         </div>)
