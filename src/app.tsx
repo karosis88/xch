c41ded41c066316223bdb059acc648ac2e8d88b9
karosis88 <kar.petrosyanpy@gmail.com>
2023-09-16 12:03:53 +0300

    Remove xch

--- src/app.tsx
+++ src/app.tsx
@@ -4,8 +4,5 @@ import Encoder from "./components/encoder"
 export default function App() {
     return <div className="app">
         <Encoder/>
-        <h3 className="special-for-khachik">
-            Հատուկ Խաչիկ Սարգսյանի համար
-        </h3>
     </div>
 }
